c658d57dc5cd4f154270367e0804fcec91c4b967
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-13 20:10:34 +0700

    Add Question24.py file

---
+++ Question24.py
@@ -0,0 +1,16 @@
+def custom_function():
+    """
+    This is a custom function that prints a message.
+    It does not take any arguments and simply prints a message when called.
+    """
+    print("This is a custom function!")
+
+print("Documentation for abs():")
+print(abs.__doc__)
+print("\nDocumentation for int():")
+print(int.__doc__)
+print("\nDocumentation for raw_input():")
+print(input.__doc__)
+
+print("\nDocumentation for custom_function():")
+print(custom_function.__doc__)
