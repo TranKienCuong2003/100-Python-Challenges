9f8b0d0fff4123f9b092d09a1e2e77e9029212fd
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-14 21:58:28 +0700

    Add Question86.py file

---
+++ Question86.py
@@ -0,0 +1,8 @@
+subjects = ["I", "You"]
+verbs = ["Play", "Love"]
+objects = ["Hockey", "Football"]
+
+for subject in subjects:
+    for verb in verbs:
+        for obj in objects:
+            print(f"{subject} {verb} {obj}")
