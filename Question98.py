13f99a2aafa58507d21d56df8486836e65437cbb
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-14 22:25:47 +0700

    Add Question98.py file

---
+++ Question98.py
@@ -0,0 +1,8 @@
+def print_even_index_characters(input_string):
+    even_index_chars = input_string[::2]
+    
+    print(even_index_chars)
+
+input_string = input("Enter a string: ")
+
+print_even_index_characters(input_string)
