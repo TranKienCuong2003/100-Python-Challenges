3afe51d6ade787a9c65cc024c10e591f0de4f47f
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-14 20:52:15 +0700

    Add Question63.py file

---
+++ Question63.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+#----------------------------------------#
+# This file is written in UTF-8 encoding, 
+# allowing it to handle Unicode characters correctly.
+#----------------------------------------#
