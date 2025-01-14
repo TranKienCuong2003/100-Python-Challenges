880bc9fed09ab52430c7402c23aae701deaaa9df
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-14 22:23:15 +0700

    Add Question97.py file

---
+++ Question97.py
@@ -0,0 +1,8 @@
+def reverse_string(input_string):
+    reversed_string = input_string[::-1]
+    
+    print(reversed_string)
+    
+input_string = input("Enter a string: ")
+
+reverse_string(input_string)
